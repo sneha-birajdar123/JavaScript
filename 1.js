e1c7374da3c5dc99cf64088a73d88cd187d24e00
sneha <neha06844@gmail.com>
2024-10-29 16:02:44 +0530

    javascript variables

---
+++ 1.js
@@ -0,0 +1,89 @@
+
+// var x = 5;
+// var y = 6;
+// var z = x + y;
+// console.log(z);
+// z = x * y
+// console.log(z);
+// z = x - y
+// console.log(z);
+// z = x/y
+// console.log(z);
+
+
+// var a = "Hello"
+// let b = "hiii"
+// const c = "bye"
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// let person = "John Doe",
+// carName = "Volvo",
+// price = 200;
+// console.log(person, carName, price);
+
+
+// var x = 5;
+// console.log(x); 
+// x = 10; 
+// console.log(x); 
+// var x = 20; 
+// console.log(x)
+
+
+
+// let y = 5;
+// console.log(y); 
+// y = 10; 
+// console.log(y)
+
+
+// const z = 5;
+// console.log(z); // Output: 5
+
+
+
+// var a = 1;
+// var b = 2;
+// console.log(a + b); 
+// a = 10;
+// b = 20;
+// console.log(a + b); 
+// var a = 100; 
+// console.log(a + b);
+
+
+
+// let firstName = "John";
+// let lastName = "Doe";
+// console.log(firstName + " " + lastName); 
+// firstName = "Jane"; 
+// console.log(firstName + " " + lastName)
+
+
+
+// const PI = 3.14;
+// console.log(PI); 
+
+// const gravity = 9.8;
+// console.log("Gravity:", gravity); 
+
+
+
+// const person = {
+//     name: "Alice",
+//     age: 25
+//   };
+//   console.log(person.name); // Output: Alice
+//   person.age = 26; // Modifying a property of person
+//   console.log(person.age)
+
+
+// let x = 8;
+// const y = 12;
+// let result = x + y;
+// console.log(result); 
+// x = 10; 
+// result = x + y;
+// console.log(result);
